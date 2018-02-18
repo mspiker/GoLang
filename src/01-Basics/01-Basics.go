d8459cd4c5271891e10c8b17b9dc976efc1bd5f4
Mark Spiker <kb8uvv@gmail.com>
2018-02-18 00:00:35 -0500

    More examples

--- src/01-Basics/01-Basics.go
+++ src/01-Basics/01-Basics.go
@@ -98,9 +98,9 @@ func IfLogic() {
 	// Variables, like v, declared by the statement are only in scope until the end of the if/else.  
 	// In this example we also demonstrate the use of else.  
 	if v := x * 2 ; v > 3 {
-		fmt.Printf("v is %d so we made it in the if statement.", v)
+		fmt.Printf("v is %d so we made it in the if statement.\n", v)
 	} else {
-		fmt.Printf("v is greater than 3 becuase it is %d", v)
+		fmt.Printf("v is greater than 3 becuase it is %d\n", v)
 	}
 }
 
@@ -119,7 +119,7 @@ func UsingSwitch() {
 			fmt.Println("Linux.")
 		default: 
 			// all other possible OS
-			fmt.Printf("%s.", os)
+			fmt.Printf("%s.\n", os)
 	}
 	
 	// Switch without a condition is the same as "switch true".  This construct can be a clean
@@ -135,9 +135,16 @@ func UsingSwitch() {
 	}
 }
 
+type Vertex struct {
+	X int
+	Y int
+}
+
+type GeoAddress struct {
+		Lat, Long float64
+	}
+
 func main() {
-	
-	
 	
 	fmt.Println("01-Basics Application")
 	fmt.Println(add(42, 13))
@@ -179,10 +186,133 @@ func main() {
 	// This func demonstrates Switch
 	UsingSwitch()
 	
-	// Deferring execution until the surrounding function returns.  
-	defer fmt.Print("world. This was an example of the defer statemnt.")
-	fmt.Print("hello ")
+	// Deferring execution until the surrounding function returns.  For more information on
+	// the defer statement see https://blog.golang.org/defer-panic-and-recover  
+	defer fmt.Print("world. This was an example of the defer statement.\n}")
+	fmt.Print("hello {\n")
 	
+	// Working with Pointers
+	// Pointers holds the memory address of a value.
+	fmt.Println("POINTER EXAMPLES")
+	q, r := 42, 2701
 	
+	// The & operator generates a pointer to its operand.
+	// The * operator denotes the pointer's underlying value
+	// AKA "dereferencing" or "indirecting"
+	p := &q 		// point to q
+	fmt.Println(*p)	// read q through the pointer
+	*p = 21			// set q through the pointer
+	fmt.Println(q)	// See the new value of q
 	
+	pr := &r		// point to r
+	fmt.Printf("The variable r is at address %v and contains %v\n ", pr, *pr)	// Print the address of r
+	
+	// Structs are collections of fields, accessing the fields using dot notation.
+	fmt.Println(Vertex{1, 2})
+	vtx := Vertex{1800, 650}
+	vtx.X = 2010
+	fmt.Println(vtx.X)
+	
+	// You can access struct fields through pointers
+	vtxPointer := &vtx
+	vtxPointer.X = 1e9
+	fmt.Printf("The struct contains %v and is at memory address %v\n", vtx, &vtxPointer)
+	
+	// Another way to declare values in a struct
+	var vtxA = Vertex{X: 1}	// Y:0 is implicit
+	var vtxB = Vertex{}		// X:0 and Y:0 is implicit
+	fmt.Println(vtxA, vtxB)
+	
+	// Arrays - declare a varable arr as an array of ten integers
+	var arr [10]int
+	arr[0] = 201
+	arr[1] = 180
+	fmt.Println(arr[0], arr[1])
+	
+	// Another way to declare an array of 6 integers
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+	
+	// Slices - Unlike arrays that have a fixed size, Slices is a dynamically-sized array. 
+	var s []int = primes[1:4]	// creates a slice including elements 1 through 3 of the prime array
+	fmt.Println(s)
+	
+	// Another way to do slices, this is like a dictionary in C#.  This is an example of a 
+	// Key-Value pair.
+	dict := []struct {
+		key int
+		value string
+	}{
+		{100, "One hundred"},
+		{200, "Two hundred"},
+		{300, "Three hundred"},	// Notice the last item has a comma after it - that's wierd
+	}
+	fmt.Println(dict)
+	fmt.Println(dict[:2])	// This will show index 0 and 1
+	
+	// Get some details on the length (len) of the slice and the capacity (cap).  
+	fmt.Printf("Dictionary statistics - len=%d cap=%d %v\n", len(dict), cap(dict), dict)
+	
+	// Checking for nil slices, nil is the same as Null
+	if dict == nil {
+		fmt.Println("Dictionary is empty!")
+	}
+	
+	// Create a slice with the Make function.  This allocates a zeroed array with a slice that 
+	// refers to that array. 
+	sMake := make([]int, 5)
+	fmt.Printf("len=%d cap=%d %v\n", len(sMake), cap(sMake), sMake)
+	
+	// More information about Slices; Usage and Internals see 
+	// https://blog.golang.org/go-slices-usage-and-internals
+	
+	// The range form of the for loop iterates over a slice or map.  When ranging over a slice,
+	// two values are returned for each iteration.  The first is the index, and the second is a 
+	// copy of the element at that index.  
+	var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+	for ndex, value := range pow {
+		fmt.Printf("2**%d = %d\n", ndex, value)
+	}
+	
+	// Skip the index or value by assigning to _, so below index is not needed, just the value. 
+	for _, value := range pow {
+		fmt.Printf("%d\n", value)
+	}
+	
+	// Maps - A map maps keys to a value (it's no a map for wayfinding)
+	var Providers map[string]GeoAddress
+	Providers = make(map[string]GeoAddress)
+	Providers["Dr. Smith"] = GeoAddress{40.32212, -74.12930}
+	Providers["Dr. Pepper"] = GeoAddress{40.23112, -73.23911}
+	fmt.Println(Providers["Dr. Smith"])
+	
+	// Another way to do the same as above
+	var Patients = map[string]GeoAddress {
+		"Mary": GeoAddress{40.21991, -71.23120},
+		"Tammy": GeoAddress{40.16281, -74.12391},
+	}
+	fmt.Println(Patients)	
+	
+	// Insert or update an element in a map
+	Patients["Mary"] = GeoAddress{1,-1}		// Updating an element
+	Patients["Bob"] = GeoAddress{8,-9}		// Inserting an element
+	fmt.Println(Patients)
+	
+	// Get an element
+	Mary := Patients["Mary"]
+	fmt.Println(Mary)
+	
+	// Delete an element
+	delete(Patients, "Bob")
+	fmt.Println(Patients)
+	
+	// Test that a key is present with a two-value assignment
+	if elem, ok := Patients["Mary"]; ok {
+		fmt.Printf("Found Mary and her location is %v in the map.\n", elem)
+	}
+	if elem, ok := Patients["Bob"]; ok {
+		fmt.Printf("Found Bob and his location is %v in the map.\n", elem)
+	} else {
+		fmt.Println("Can't find Bob!!")
+	}
 }
--- src/01-Basics/02-MongoDb.go
+++ src/01-Basics/02-MongoDb.go
@@ -1,10 +1,24 @@
 package main 
 
 import (
-
+	"time"
+	"log"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
+
+type Profile struct {
+	Name		string		`json: "username"`
+	Password	string		`json: "password"`
+	Age			int			'json: "age"`
+	LastUpdated	time.Time
+}
 
 func GetDB() string {
 	return "MongoDB"
+}
+
+func GetProfiles() []Profile {
+	session, err := mgo.Dial("")
 }
 
